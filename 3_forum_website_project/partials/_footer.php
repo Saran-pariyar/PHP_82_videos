f2662eceba22236aed1ffd7d68703bd7e974a88c
Saran Pariyar <86418083+Saran-pariyar@users.noreply.github.com>
2021-11-12 17:31:29 +0545

    Delete 3_forum_website_project directory

--- 3_forum_website_project/partials/_footer.php
+++
@@ -1,9 +0,0 @@
-
-<style>
-#footer{
-    margin-top: 10%;
-    min-width: 100%;
-}
-</style>
-
-<div id="footer" class="text-light bg-dark text-center py-3 "> Copyright iDiscuss Coding Forum 2021 | All rights reserved </div>
--- 3_forum_website_project/partials/_logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-echo 'logging you out...please wait...';
-
-session_destroy();
-session_unset();
-//after doing the above action, our username and password will be removed and we will redirect to index.php
-header("Location:/CWH-php/3_forum_website_project/index.php")
-?>
